4ab713701a530a6dcd7505d47ee10a7bafd80ad7
Kent <slavidkent@gmail.com>
2022-04-11 23:32:24 +0800

    Complete exercise 04 05

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,16 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (arr, ...dataToRemove) {
+    let numberIndex;
+    for (let i = 0; i < dataToRemove.length; i++) {
+        numberIndex = arr.indexOf(dataToRemove[i]);
+        if (numberIndex===-1){
+            continue;
+        }else{
+            arr.splice(numberIndex,1);
+        }
+    }
+    return arr;
 };
 
+removeFromArray([2, 3, 4, 5, 6, 7], 4, 2);
 // Do not edit below this line
 module.exports = removeFromArray;
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,20 @@
-const sumAll = function() {
+const sumAll = function (startVal, endVal) {
+    let sum = 0;
+    
+    if (startVal < 0 || endVal < 0 || typeof startVal !== 'number' || typeof endVal !== 'number') {
+        return 'ERROR';
+    }
+    if (startVal > endVal) {
+        [startVal, endVal] = [endVal, startVal];
+    }
 
+    for (let i = startVal; i <= endVal; i++) {
+        sum += i;
+    }
+    return sum;
 };
+
+sumAll(100, 88);
 
 // Do not edit below this line
 module.exports = sumAll;
