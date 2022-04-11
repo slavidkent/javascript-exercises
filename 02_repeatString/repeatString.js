0640980eecc03210c8bc465e46b01d4183e505a0
Kent <slavidkent@gmail.com>
2022-04-11 22:35:36 +0800

    Complete exercise 01 02 03

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(word, num) {
+    if (num<0){
+        return 'ERROR'
+    }
+    let words='';
+    for (let i=0; i<num;i++){
+        words+=word;
+    }
+    return words;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function (word) {
+    let wordArr = [...word];
+    let reversedArr = [];
+    for (let i = 0; i < wordArr.length; i++) {
+        reversedArr.unshift(wordArr[i]);
+    }
+    return reversedArr.join('');
 };
 
 // Do not edit below this line
