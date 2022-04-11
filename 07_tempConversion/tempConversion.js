bd7f0837e88f555fcd7e0f960de3a628a159de0d
Kent <slavidkent@gmail.com>
2022-04-12 07:45:26 +0800

    Complete exercise 06 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,15 @@
-const ftoc = function() {
-
+const ftoc = function (fahr) {
+    let celc = ((fahr - 32) * 5) / 9;
+    return Math.round(celc * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function (celc) {
+    let fahr = (celc * 9) / 5 + 32;
+    return Math.round(fahr * 10) / 10;
 };
 
 // Do not edit below this line
 module.exports = {
-  ftoc,
-  ctof
+    ftoc,
+    ctof,
 };
