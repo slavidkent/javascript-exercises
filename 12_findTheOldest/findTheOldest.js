9d13104807319f0cf6d4e55a1fd8ce95688575c8
Kent <slavidkent@gmail.com>
2022-04-19 21:07:57 +0800

    Complete 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function (peoples) {
+    return peoples.reduce((oldestGuy, people) => {
+        if (calculateAge(oldestGuy) < calculateAge(people)) {
+            oldestGuy = people;
+        }
+        return oldestGuy;
+    });
+};
 
+const calculateAge = (obj) => {
+    let age = 0;
+    if (obj.yearOfDeath === undefined) {
+        age = new Date().getFullYear() - obj.yearOfBirth;
+    } else {
+        age = obj.yearOfDeath - obj.yearOfBirth;
+    }
+    console.log(`age of ${obj.name} is ${age}`)
+    return age;
 };
 
 // Do not edit below this line
