42abc398f8b9675d7419a5a120b712787251aec0
Kent <slavidkent@gmail.com>
2022-04-19 17:15:30 +0800

    Complete 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    alphabetOnly = word.replace(/[\s\W]/g, '').toLowerCase();
+    reversedWord = alphabetOnly.split('').reverse().join('');
+    return alphabetOnly === reversedWord;
 };
 
 // Do not edit below this line
