19f0e5ef75431dd69cc7e75b32c02138599084f1
Kent <slavidkent@gmail.com>
2022-04-19 17:26:13 +0800

    Complete 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    if (number<=0){
+        return 'OOPS'
+    }
+    else if(number<=2){
+        return 1
+    }else{
+        return fibonacci(number-1)+fibonacci(number-2)
+    }
 };
+
 
 // Do not edit below this line
 module.exports = fibonacci;
